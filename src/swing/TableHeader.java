1452398eb51a35b03878ee31280ef2c5ee442662
slakcer <122334224+slakcer@users.noreply.github.com>
2023-11-18 21:12:35 +0800

    Added additional files

---
+++ src/swing/TableHeader.java
@@ -0,0 +1,27 @@
+package swing;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
+
+public class TableHeader extends JLabel {
+
+    public TableHeader(String text) {
+        super(text);
+        setOpaque(true);
+        setBackground(Color.WHITE);
+        setFont(new Font("Roboto Bk", 1, 20));
+        setForeground(new Color(0, 0, 0));
+        setBorder(new EmptyBorder(0, 0, 0, 0));
+        setHorizontalAlignment(JLabel.CENTER);
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        super.paintComponent(grphcs);
+        grphcs.setColor(new Color(230, 230, 230));
+        grphcs.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
+    }
+}
